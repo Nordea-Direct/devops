bc217e3007ed635cc6829ab1ae701fc4d4ee57ce
Geir Inge <Geir-Inge.Hosteng@gjensidige.no>
2018-12-19 11:50:44 +0100

    WIP

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -23,7 +23,7 @@ $global:group,$global:artifact = $app.Split(':',2)
 #konstanter
 #http://nexus/service/local/repositories/snapshots/content/no/gjensidige/bank/bygg-release-deploy-poc/0.1.2-SNAPSHOT/bygg-release-deploy-poc-0.1.2-20181219.102459-9.zip
 $NEXUS_BASE = "http://nexus/service/local/repositories/releases/content/"
-$NEXUS_SNAPSHOT_BASE = "http://nexus/service/local/artifact/maven/redirect?r=snapshot&"
+$NEXUS_SNAPSHOT_BASE = "http://nexus/service/local/artifact/maven/redirect?r=snapshots&"
 $TMP_DIR_BASE = "D:\devops"
 $BASE_PATH = "D:\gbapi"
 $NOTIFY_SLEEP_TIME = 3 * 60 * 1000 # 3 minutter
