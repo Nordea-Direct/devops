0cd45db35f2c5d44b03fa46d431401d59c3e6bd2
Georg Sirnes Lundesgaard <georg.sirnes-lundesgaard@gjensidigebank.no>
2019-09-30 13:52:14 +0200

    NORE-124: feat: lagt til script for Apache-deploy

---
+++ deploy/deployApache.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern('.+:.+')]
+    [string]$app,
+
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern('^\d+\.\d+(\.\d+)?(-SNAPSHOT)?$')]
+    [string]$version,
+
+    [Parameter(Mandatory=$true)]
+    [string]$healthUrl,
+
+    [Parameter(Mandatory=$true)]
+    [string]$linkTilReleaseDok
+)
