a83deaff9365857d015492c7c00b811813eadd78
Georg Sirnes Lundesgaard <georg.sirnes-lundesgaard@gjensidigebank.no>
2019-10-07 14:34:17 +0200

    NORE-124: feat: script for Apache-deploy

--- deploy/deployApache.ps1
+++ deploy/deployApache.ps1
@@ -34,7 +34,7 @@ function stopp_service($serviceName) {
     $service = 0
 
     # kjører servicen ?
-    skriv_steg "sjekker om $serviceName kjoerer"
+    Write-Output "sjekker om $serviceName kjoerer"
 
     try {
         $service = Get-Service -Name $serviceName -EA SilentlyContinue
@@ -56,7 +56,7 @@ function stopp_service($serviceName) {
 
     # hvis service kjører - stopp service
     if ($kjorer) {
-        skriv_steg "stopper servicen $serviceName"
+        Write-Output "stopper servicen $serviceName"
 
         Stop-Service -Name $serviceName -EA SilentlyContinue
 
@@ -65,6 +65,8 @@ function stopp_service($serviceName) {
         if (sjekkOmKjoerer($serviceName)) {
             Write-Output "feilet med stoppe servicen $serviceName, gir opp"
             exit 1
+        } else {
+            Write-Output "service $serviceName stoppet"
         }
     }
 }
@@ -91,6 +93,17 @@ function kopier_filer($src_dir, $dest_dir) {
     }
 }
 
+function slett_mappe($dir) {
+    try {
+        $output = Remove-Item -Recurse -Force $dir
+        Write-Output "slettet mappe: $dir"
+    } catch {
+        $feilmelding= hentFeilmelding($_)
+        Write-Output "feilet med aa slette mappen $dir : $feilmelding"
+        exit 1
+    }
+}
+
 try {
     $global:ServiceErIEnUgyldigState = $false
 
@@ -112,130 +125,39 @@ try {
 
     skriv_steg "kopierer inn ny config"
 
-    try {
-        $output = Remove-Item -Recurse -Force $CONF_DIR
-        Write-Output "slettet mappe: $CONF_DIR"
-    } catch {
-        $feilmelding= hentFeilmelding($_)
-        Write-Output "feilet med aa slette mappen $CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        $output = New-Item -ItemType Directory -Force -Path $CONF_DIR
-        Write-Output "opprettet mappe: $CONF_DIR"
-    } catch {
-        $feilmelding = hentFeilmelding($_)
-        Write-Output "feilet med aa opprette mappen $CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        Copy-Item -Path "$CONF_BASE_DIR\*" -Destination $CONF_DIR -Recurse -force
-        Write-Output "kopiert filer fra $CONF_BASE_DIR til $CONF_DIR"
-    } catch {
-        $feilmelding = hentFeilmelding($_)
-        Write-Output "feilet med aa kopiere filer fra $CONF_BASE_DIR til $CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        Copy-Item -Path "$UPLOADS_CONF_DIR\*" -Destination $CONF_DIR -Recurse -force
-        Write-Output "kopiert filer fra $UPLOADS_CONF_DIR til $CONF_DIR"
-    } catch {
-        $feilmelding = hentFeilmelding($_)
-        Write-Output "feilet med aa kopiere filer fra $UPLOADS_CONF_DIR til $CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        $output = Remove-Item -Recurse -Force $UPLOADS_CONF_DIR
-        Write-Output "slettet mappe: $UPLOADS_CONF_DIR"
-    } catch {
-        $feilmelding= hentFeilmelding($_)
-        Write-Output "feilet med aa slette mappen $UPLOADS_CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        $output = New-Item -ItemType Directory -Force -Path $UPLOADS_CONF_DIR
-        Write-Output "opprettet mappe: $UPLOADS_CONF_DIR"
-    } catch {
-        $feilmelding = hentFeilmelding($_)
-        Write-Output "feilet med aa opprette mappen $UPLOADS_CONF_DIR : $feilmelding"
-        exit 1
-    }
-
-    try {
-        $output = New-Item -ItemType Directory -Force -Path $UPLOADS_CONF_DIR\extra
-        Write-Output "opprettet mappe: $UPLOADS_CONF_DIR\extra"
-    } catch {
-        $feilmelding = hentFeilmelding($_)
-        Write-Output "feilet med aa opprette mappen $UPLOADS_CONF_DIR\extra : $feilmelding"
-        exit 1
-    }
+    slett_mappe $CONF_DIR
+    opprett_mappe $CONF_DIR
+    kopier_filer $CONF_BASE_DIR, $CONF_DIR
+    kopier_filer $UPLOADS_CONF_DIR, $CONF_DIR
+    slett_mappe $UPLOADS_CONF_DIR
+    opprett_mappe $UPLOADS_CONF_DIR
+    opprett_mappe $UPLOADS_CONF_DIR\extra
 
     skriv_steg "starter service $SERVICE_NAME"
     
     Start-Service -Name $SERVICE_NAME
     Write-Output "service $SERVICE_NAME startet"
 
-    skriv_steg "sjekker at service $SERVICE_NAME kjorer"
-
     sleep 2
 
-    if (sjekkOmKjoerer($SERVICE_NAME)) {
-        Write-Output "service $SERVICE_NAME kjorer"
+    skriv_steg "sjekker at service $SERVICE_NAME kjorer"
 
+    if (sjekkOmKjoerer($SERVICE_NAME)) {
         $global:ServiceErIEnUgyldigState = $false
 
-        skriv_steg "sletter backup Apache Httpd-config"
-
-        try {
-            $output = Remove-Item -Recurse -Force $CONF_BACKUP_DIR
-            Write-Output "slettet mappe: $CONF_BACKUP_DIR"
-        } catch {
-            $feilmelding= hentFeilmelding($_)
-            Write-Output "feilet med aa slette mappen $CONF_BACKUP_DIR : $feilmelding"
-            exit 1
-        }
-
-        skriv_steg "SUKSESS: config for $SERVICE_NAME oppdatert"
+        Write-Output "service $SERVICE_NAME kjorer"
     } else {
-        Write-Output "service $SERVICE_NAME kjorer IKKE"
-
         $global:ServiceErIEnUgyldigState = $true
+
+        Write-Output "service $SERVICE_NAME kjorer IKKE"
     }
 } finally {
     if ($ServiceErIEnUgyldigState) {
         skriv_steg "deploy feiler, prover a legge tilbake gammel versjon"
 
-        try {
-            $output = Remove-Item -Recurse -Force $CONF_DIR
-            Write-Output "slettet mappe: $CONF_DIR"
-        } catch {
-            $feilmelding= hentFeilmelding($_)
-            Write-Output "feilet med aa slette mappen $CONF_DIR : $feilmelding"
-            exit 1  
-        }
-
-        try {
-            $output = New-Item -ItemType Directory -Force -Path $CONF_DIR
-            Write-Output "opprettet mappe: $CONF_DIR"
-        } catch {
-            $feilmelding = hentFeilmelding($_)
-            Write-Output "feilet med aa opprette mappen $CONF_DIR : $feilmelding"
-            exit 1
-        }
-
-        try {
-            Copy-Item -Path "$CONF_BACKUP_DIR\*" -Destination $CONF_DIR -Recurse -force
-            Write-Output "kopiert filer fra $CONF_BACKUP_DIR til $CONF_DIR"
-        } catch {
-            $feilmelding = hentFeilmelding($_)
-            Write-Output "feilet med aa kopiere filer fra $CONF_BACKUP_DIR til $CONF_DIR : $feilmelding"
-            exit 1
-        }
+        slett_mappe $CONF_DIR
+        opprett_mappe $CONF_DIR
+        kopier_filer $CONF_BACKUP_DIR, $CONF_DIR
 
         skriv_steg "starter service $SERVICE_NAME"
         
@@ -249,22 +171,17 @@ try {
         if (sjekkOmKjoerer($SERVICE_NAME)) {
             Write-Output "service $SERVICE_NAME kjorer"
 
-            skriv_steg "sletter backup Apache Httpd-config"
+            slett_mappe $CONF_BACKUP_DIR
 
-            try {
-                $output = Remove-Item -Recurse -Force $CONF_BACKUP_DIR
-                Write-Output "slettet backupmappe: $CONF_BACKUP_DIR"
-            } catch {
-                $feilmelding= hentFeilmelding($_)
-                Write-Output "feilet med aa slette mappen $CONF_BACKUP_DIR : $feilmelding"
-                exit 1
-            }
-
-            skriv_steg "SEMI-FEIL: config rullet tilbake til forrige versjon"
+            Write-Output "SEMI-FEIL: config rullet tilbake til forrige versjon"
         } else {
             Write-Output "service $SERVICE_NAME kjorer IKKE"
 
-            skriv_steg "FEIL: rollback til eldre versjon feilet. Service $SERVICE_NAME startet ikke"
+            Write-Output "FEIL: rollback til eldre versjon feilet. Service $SERVICE_NAME startet ikke"
         }
+    } else {
+        slett_mappe $CONF_BACKUP_DIR
+
+        Write-Output "SUKSESS: config for $SERVICE_NAME oppdatert"
     }
 }
