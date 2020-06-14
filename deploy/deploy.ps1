dd0e76de99b7582fa801d99ff582d66b8bb8a7bc
Espen Erlandesen <Espen.Erlandsen@gjensidige.no>
2020-06-14 21:47:12 +0200

    Invoke-WebRequest i stedet for WebClient.DownloadString

--- deploy/deploy.ps1
+++ deploy/deploy.ps1
@@ -151,7 +151,7 @@ function Install-Application([string]$ApplicationDirectory, [string]$HealthUri, 
 function Test-ApplicationStatusUp([string]$HealthUri) {
     try {
         Write-SubStep "Sending request to $HealthUri"
-        $healthRepsonse = $wc.DownloadString($HealthUri) | ConvertFrom-Json
+        $healthRepsonse = Invoke-WebRequest $HealthUri | ConvertFrom-Json
         Write-SubStep "Reading response"
         return $healthRepsonse.status -eq "UP"
     }
